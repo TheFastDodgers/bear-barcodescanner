2a6d253f38ddb1df014f8c3e0571ca0773aa8642
lurriel <tpodobinski@gmail.com>
2021-03-14 19:00:19 +0100

    barcode update

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -7,8 +7,16 @@ interface ContainerProps {
 const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
   return (
     <div className="container">
-      <strong>{name}</strong>
-      <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <strong>{name}1</strong><br/>
+        <strong>{name}2</strong><br/>
+        <strong>{name}3</strong><br/>
+        <strong>{name}4</strong><br/>
+        <strong>{name}5</strong><br/>
+        <strong>{name}6</strong><br/>
+        <strong>{name}7</strong><br/>
+        <strong>{name}8</strong><br/>
+        <strong>{name}</strong><br/>
+        <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
     </div>
   );
 };
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,25 +1,62 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
+// const Tab1: React.FC = () => {
+//   return (
+//     <IonPage>
+//       <IonHeader>
+//         <IonToolbar>
+//           <IonTitle>Tab 1</IonTitle>
+//         </IonToolbar>
+//       </IonHeader>
+//       <IonContent fullscreen>
+//         <IonHeader collapse="condense">
+//           <IonToolbar>
+//             <IonTitle size="large">Tab 1</IonTitle>
+//           </IonToolbar>
+//         </IonHeader>
+//         <ExploreContainer name="Tab 1 page" />
+//       </IonContent>
+//     </IonPage>
+//   );
+// };
+
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import {useState} from "react";
+import {Photo} from "../hooks/usePhotoGallery";
+
 const Tab1: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
-      </IonContent>
-    </IonPage>
-  );
+    const [pointer, setPointer] = useState<string>('What');
+    const [scanned, setScanned] = useState<string>('data test');
+    const openScanner = async () => {
+        setScanned('waiting');
+        const data = await BarcodeScanner.scan().then((data)=>{
+            setPointer('promise done');
+            setScanned(data.text);
+        });
+        // setScanned(data.text);
+        // console.log(`Barcode data: ${data.text}`);
+    };
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Tab 1</IonTitle>
+                </IonToolbar>
+
+            </IonHeader>
+            <IonContent>
+                Barcode
+                <br/>
+                {pointer}
+                <br/>
+                {scanned}
+                <br/>
+                <IonButton onClick={openScanner}>Scan barcode</IonButton>
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Tab1;
