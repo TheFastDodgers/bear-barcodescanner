75d9d6448ee18742e085347c5af9786ed77898f9
lurriel <tpodobinski@gmail.com>
2021-03-17 18:52:00 +0100

    barcode update 2

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,43 +1,15 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+
 import './Tab1.css';
 
-// const Tab1: React.FC = () => {
-//   return (
-//     <IonPage>
-//       <IonHeader>
-//         <IonToolbar>
-//           <IonTitle>Tab 1</IonTitle>
-//         </IonToolbar>
-//       </IonHeader>
-//       <IonContent fullscreen>
-//         <IonHeader collapse="condense">
-//           <IonToolbar>
-//             <IonTitle size="large">Tab 1</IonTitle>
-//           </IonToolbar>
-//         </IonHeader>
-//         <ExploreContainer name="Tab 1 page" />
-//       </IonContent>
-//     </IonPage>
-//   );
-// };
-
-import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 import {useState} from "react";
-import {Photo} from "../hooks/usePhotoGallery";
+
+import BarcodeScannerComponent from "react-webcam-barcode-scanner";
 
 const Tab1: React.FC = () => {
-    const [pointer, setPointer] = useState<string>('What');
-    const [scanned, setScanned] = useState<string>('data test');
-    const openScanner = async () => {
-        setScanned('waiting');
-        const data = await BarcodeScanner.scan().then((data)=>{
-            setPointer('promise done');
-            setScanned(data.text);
-        });
-        // setScanned(data.text);
-        // console.log(`Barcode data: ${data.text}`);
-    };
+
+    const [ data, setData ] = useState<any>('Not Found');
+
     return (
         <IonPage>
             <IonHeader>
@@ -47,13 +19,17 @@ const Tab1: React.FC = () => {
 
             </IonHeader>
             <IonContent>
-                Barcode
-                <br/>
-                {pointer}
-                <br/>
-                {scanned}
-                <br/>
-                <IonButton onClick={openScanner}>Scan barcode</IonButton>
+
+                <BarcodeScannerComponent
+                    width={500}
+                    height={500}
+                    onUpdate={(err, result: any) => {
+                        if (result) setData(result.text)
+                        else setData('Not Found')
+                    }}
+                />
+                <p>{data}</p>
+
             </IonContent>
         </IonPage>
     );
